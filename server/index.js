df1c3d3c6edb0f1905ee331072e1ecbc722b704b
Vatsal <chawlavatsal27@gmail.com>
2023-08-06 15:19:49 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,43 +18,21 @@ import User from "./models/User.js";
 import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
 
-dotenv.config();
-
+/* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+dotenv.config();
 const app = express();
-
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-
-// CORS configuration
-const allowedOrigins = ["http://localhost:3000"];
-app.use(
-  cors({
-    origin: allowedOrigins,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  })
-);
-
-// Serve static assets
+app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-// Route for user registration with profile picture upload
-app.post("/auth/register", upload.single("picture"), register);
-
-// Route for creating a post with an optional picture upload (requires authentication)
-app.post("/posts", verifyToken, upload.single("picture"), createPost);
-
-app.use("/auth", authRoutes);
-app.use("/users", userRoutes);
-app.use("/posts", postRoutes);
-
+/* FILE STORAGE */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public/assets");
@@ -63,12 +41,19 @@ const storage = multer.diskStorage({
     cb(null, file.originalname);
   },
 });
-
 const upload = multer({ storage });
 
-const PORT = process.env.PORT || 6001;
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
-mongoose.set("strictQuery", true);
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -78,11 +63,7 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     /* ADD DATA ONE TIME */
-    // Uncomment the following lines to insert sample data into the database on startup
     // User.insertMany(users);
     // Post.insertMany(posts);
   })
-  .catch((error) => {
-    console.log(`${error} did not connect`);
-
-  });
+  .catch((error) => console.log(`${error} did not connect`));
